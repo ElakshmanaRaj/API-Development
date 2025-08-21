18e5040e8e2617f4a3776b7b13dae3a502333c72
ElakshmanaRaj <elakshmanabca07@gmail.com>
2025-08-21 14:37:12 +0530

    fix issue

--- config/database.js
+++ config/database.js
@@ -1,15 +1,19 @@
-
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.DB_URL, {})
-        console.log(`Database Connected: ${conn.connection.host}, ${conn.connection.name}`);
-        
-    } catch (error) {
-        console.error(`Database Connection Failed: ${error.message}`)
-        process.exit(1)
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(
+      `Database Connected: ${conn.connection.host} / ${conn.connection.name}`
+    );
+  } catch (error) {
+    console.error(`Database Connection Failed: ${error.message}`);
+    process.exit(1); // Stop the app if DB fails
+  }
+};
 
 module.exports = connectDB;
